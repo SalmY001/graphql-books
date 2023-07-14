bf34fb90dfab08914e8d84ad859709b3c44dea73
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-07-14 23:46:43 +0100

    start updating mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,18 +1,42 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-  mutation addUser($profileId: ID!, $skill: String!) {
-    addUser(profileId: $profileId, skill: $skill) {
+mutation addUser($username: String, $email: String, $password: String) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
       _id
-      name
-      skills
+      bookCount
+      email
+      savedBooks {
+        _id
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
     }
   }
+}
 `;
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($email: String!, $password: String!) {
+    saveBook(email: $email, password: $password) {
       token
       user {
         _id
